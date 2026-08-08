fdb79626b4c6edde21ac62e47803d5e32aa80484
juniordev203 <ttk11092k3@gmail.com>
2026-08-08 18:09:13 +0700

    Add glass3d glass background across the site layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="flex min-h-screen flex-col">
+    <div class="glass3d fixed inset-0 pointer-events-none" aria-hidden="true" />
     <a
       href="#main-content"
-      class="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-[60] focus:border focus:border-primary focus:bg-background focus:px-4 focus:py-2 focus:text-label focus:text-primary"
+      class="relative z-10 sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-[60] focus:border focus:border-primary focus:bg-background focus:px-4 focus:py-2 focus:text-label focus:text-primary"
     >
       Skip to content
     </a>
-    <LayoutAppHeader />
-    <main
-      id="main-content"
-      tabindex="-1"
-      class="container-site w-full flex-grow pt-28 pb-section-mobile md:pb-section outline-none"
-    >
-      <slot />
-    </main>
-    <LayoutAppFooter />
+    <div class="relative z-10 flex flex-grow flex-col">
+      <LayoutAppHeader />
+      <main
+        id="main-content"
+        tabindex="-1"
+        class="container-site w-full flex-grow pt-28 pb-section-mobile md:pb-section outline-none"
+      >
+        <slot />
+      </main>
+      <LayoutAppFooter />
+    </div>
   </div>
 </template>
